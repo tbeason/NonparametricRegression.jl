1e3e903dc45219ea0e4e50ccc75b6ed0c5890d1e
Tyler Beason <tbeas12@gmail.com>
2021-12-22 10:50:56 -0500

    toml updates

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,42 @@
-using NonparametricRegression, Distributions
+using NonparametricRegression
+using Distributions, KernelFunctions, LinearAlgebra
+using StableRNGs
 using Test
+
+
+rng = StableRNG(123)
+vn = randn(rng,100)
+vn[1] = 0.0
+vm = rand(rng,20,20)
 
 # write tests here
 
 @testset "NormalKernel" begin 
     nk1 = NormalKernel(1)
-    @test nk1.(0:2,0) ≈ pdf.(Normal(0,1),0:2)
+    @test nk1.(vn,0) ≈ pdf.(Normal(0,1),vn)
 
     nk01 = NormalKernel(0.1)
-    @test nk01.(0:2,0) ≈ pdf.(Normal(0,0.1),0:2)
+    @test nk01.(vn,0) ≈ pdf.(Normal(0,0.1),vn)
+
+    @test kernelmatrix(nk1,vn,vn) ≈ nk1.(vn,vn')
 end
+
+
+@testset "Utilities" begin
+
+    @test NonparametricRegression.normalizecols(vm) ≈ vm ./ sum(vm,dims=1) 
+    
+    vm0 = copy(vm)
+    NonparametricRegression.zerodiagonal!(vm0)
+    @test tr(vm0) ≈ 0
+end
+
+
+@testset "KDE" begin
+    @test NonparametricRegression.silvermanbw(vn) ≈ 0.35984995544649584
+
+    kd = NonparametricRegression.densityestimator(randn(rng,2000),[0.0,1.0])
+
+    @test kd ≈ pdf.(Normal(0,1),[0.0,1.0]) rtol=1e-2
+end
+
